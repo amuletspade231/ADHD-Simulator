2f5a8c41474efc4ba220e0043566e6a01330424f
Shellmadinger <48625443+Shellmadinger@users.noreply.github.com>
2024-09-14 14:20:50 -0400

    Developed Game Skeleton
    Made a basic skeleton of the main gameplay loop. Backend of this is ugly as sin though.

--- ADHDSimulator/Assets/Scripts/ButtonManager.cs
+++ ADHDSimulator/Assets/Scripts/ButtonManager.cs
@@ -4,14 +4,59 @@ using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
 {
+    //Button Manager provides functions that will be needed for our buttons, mainly two, Randon Outcome and Advance GameState
     [SerializeField] GameManager currentState;
-    
 
     // Start is called before the first frame update
-    public void RandomOutcome()
+    public void RandomOutcomeWakeUp()
+    {
+        //This function gets the random result from RandomResult and gives the appropriate outcome
+        //As I mentioned in Game Manager, we're gonna need a outcome for all options for the game PER state.
+        //This means button manager will functions like RandomeOutComeWork1, RandomeOutComeWork2, etc.
+        //All of these functions work the same, but need different panels for them. I'll probably have a basic skeleton of the game by the time you see this
+        currentState.RandomResult();
+        currentState.ShowOutCome(currentState.RandomResult(), currentState.wakeUpGreat, currentState.wakeUpNormal,currentState.wakeUpFail);
+    }
+
+    public void RandomOutcomeFood()
     {
         currentState.RandomResult();
-        currentState.ShowOutCome(currentState.RandomResult(), currentState.GreatSuccess, currentState.NormalSuccess,currentState.Failure);
+        currentState.ShowOutCome(currentState.RandomResult(), currentState.foodGreat, currentState.foodNormal, currentState.foodFail);
+    }
+
+    public void RandomOutcomeWork()
+    {
+        currentState.RandomResult();
+        currentState.ShowOutCome(currentState.RandomResult(), currentState.workGreat, currentState.workNormal, currentState.workFail);
+    }
+
+    public void AdvanceGameState()
+    {
+        //When advancing the state, we disable all panels and increase the game state by 1 (which is what will replace the debug.log here.
+        //To avoid having more variables then necessary, we can tuck all of the options and outcome panels in one empty (so a Work object would have all of the options
+        //and outcomes associated with work) Once we're done with it, we can disable just that object and also disable everything beneath it.
+        
+        Debug.Log("Game State advanced");
+
+        //So apparently if you reenable an object, the current result will also reenable. tbh, I'm not making advance game state functions per panel as this
+        //feels very tedious anyways, so I'm gonna disable them all at once.
+        //I'm sure there's a far better way to do this, I just haven't figure it out yet.
+        currentState.gameState += 1;
+        currentState.continueButton.SetActive(false);
+        currentState.wakeUpGreat.SetActive(false);
+        currentState.wakeUpNormal.SetActive(false);
+        currentState.wakeUpFail.SetActive(false);
+        currentState.foodGreat.SetActive(false);
+        currentState.foodNormal.SetActive(false);
+        currentState.foodFail.SetActive(false);
+        currentState.workGreat.SetActive(false);
+        currentState.workNormal.SetActive(false);
+        currentState.workFail.SetActive(false);
+    }
+
+    public void EndGame()
+    {
+        currentState.gameState = 0;
     }
  
 }
--- ADHDSimulator/Assets/Scripts/GameManager.cs
+++ ADHDSimulator/Assets/Scripts/GameManager.cs
@@ -4,43 +4,106 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject GreatSuccess;
-    public GameObject NormalSuccess;
-    public GameObject Failure;
+    //The continue button is used to advance the game state and bring new options for the player. You can delay the apperance of the button with a coroutine
+    public GameObject continueButton;
+    //The following panels are show the outcome of option. Given how the game works and how the code is set up...
+    //You'll need three of these variables for every option in the game, so be prepared for a big inspector
+    
+    //Case in point. Look at all those variables!
+    public GameObject startScreen;
+    public GameObject wakeUpScene;
+    public GameObject wakeUpGreat;
+    public GameObject wakeUpNormal;
+    public GameObject wakeUpFail;
+    public GameObject foodScene;
+    public GameObject foodGreat;
+    public GameObject foodNormal;
+    public GameObject foodFail;
+    public GameObject workScene;
+    public GameObject workGreat;
+    public GameObject workNormal;
+    public GameObject workFail;
+    public GameObject endScreen;
+    //The game state is just that, the current state of the game and how we can control the pacing
+    public int gameState;
     // Start is called before the first frame update
     private void Start()
     {
-        GreatSuccess.SetActive(false);
-        NormalSuccess.SetActive(false);
-        Failure.SetActive(false);
+        //When starting, we will need all of the uneccessary panels to be off, so that we can turn then on at the righ time.
+        startScreen.SetActive(true);
+        wakeUpScene.SetActive(false);
+        foodScene.SetActive(false);
+        workScene.SetActive(false);
+        continueButton.SetActive(false);
+        endScreen.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(gameState == 0)
+        {
+            startScreen.SetActive(true);
+            wakeUpScene.SetActive(false);
+            foodScene.SetActive(false);
+            workScene.SetActive(false);
+            endScreen.SetActive(false);
+        }
+
+        if(gameState == 1)
+        {
+            startScreen.SetActive(false);
+            wakeUpScene.SetActive(true);
+        }
+
+        if(gameState == 2)
+        {
+            wakeUpScene.SetActive(false);
+            foodScene.SetActive(true);
+        }
+
+        if (gameState == 3)
+        {
+            foodScene.SetActive(false);
+            workScene.SetActive(true);
+        }
+
+        if (gameState == 4)
+        {
+            workScene.SetActive(false);
+            endScreen.SetActive(true);
+        }
     }
     public int RandomResult()
     {
-        int result = Random.Range(1, 10);
+        //Randome Results just returns a number between 1 and 6. The main mechanic of our game
+        int result = Random.Range(1, 6);
         return result;
     }
 
     public void ShowOutCome(int result, GameObject GreatSuccess, GameObject NormalSuccess, GameObject Failure)
     {
-        if (result >= 8)
+        //Show OutCome display to our player the result of the option, and then shows the continue button afterwards.
+        if (result >= 5)
         {
             GreatSuccess.SetActive(true);
             NormalSuccess.SetActive(false);
             Failure.SetActive(false);
         }
 
-        if(result<=7 && result >= 4)
+        if(result<=4 && result >= 3)
         {
             GreatSuccess.SetActive(false);
             NormalSuccess.SetActive(true);
             Failure.SetActive(false);
         }
 
-        if (result <= 3)
+        if (result <= 2)
         {
             GreatSuccess.SetActive(false);
             NormalSuccess.SetActive(false);
             Failure.SetActive(true);
         }
+
+        continueButton.SetActive(true);
     }
 }
